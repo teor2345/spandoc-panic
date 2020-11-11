2f57a83c89e04b5b57f0f8da253d90ebe84849da
teor <teor@riseup.net>
2020-11-12 07:52:39 +1000

    Initial version

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#[spandoc::spandoc]
+fn _dont_panic_return_value() -> i32 {
+    42
+}
+
+#[spandoc::spandoc]
+fn panic_return_value_with_match() -> i32 {
+    match 42 {
+        _ => {},
+    };
+
+    42
+}
+
+#[spandoc::spandoc]
+fn _panic_no_arg_or_return() {}
+
+#[spandoc::spandoc]
+fn _panic_arg_but_no_return(_arg: i32) {}
+
+#[spandoc::spandoc]
+fn main() {
+}
