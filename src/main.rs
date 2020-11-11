e4a719c169b7842279d080d7b78b3515aa7c1131
teor <teor@riseup.net>
2020-11-12 08:34:18 +1000

    Avoid unused function warnings

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn _dont_panic_return_type_value() -> i32 {
 fn _panic_return_type_no_value() -> i32 {}
 
 #[spandoc::spandoc]
-fn dont_panic_return_unit_type_value() -> () {
+fn _dont_panic_return_unit_type_value() -> () {
     ()
 }
 
@@ -15,7 +15,7 @@ fn dont_panic_return_unit_type_value() -> () {
 fn _panic_return_unit_type_no_value() -> () {}
 
 #[spandoc::spandoc]
-fn dont_panic_no_return_type_but_unit_value() {
+fn _dont_panic_no_return_type_but_unit_value() {
     ()
 }
 
@@ -23,14 +23,14 @@ fn dont_panic_no_return_type_but_unit_value() {
 fn _panic_no_return_type() {}
 
 #[spandoc::spandoc]
-fn dont_panic_match_non_empty_block() {
+fn _dont_panic_match_non_empty_block() {
     match 42 {
         _ => {42},
     };
 }
 
 #[spandoc::spandoc]
-fn panic_match_empty_block() {
+fn _panic_match_empty_block() {
     match 42 {
         _ => {},
     };
