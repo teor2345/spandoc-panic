30ec88ff691f48246525070796b8db8df1e02903
teor <teor@riseup.net>
2020-11-12 08:15:51 +1000

    Add an extra success case

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ fn _dont_panic_return_value() -> i32 {
 }
 
 #[spandoc::spandoc]
+fn _dont_panic_arg_return_value(_arg: i32) -> i32 {
+    42
+}
+
+#[spandoc::spandoc]
 fn panic_return_value_with_match() -> i32 {
     match 42 {
         _ => {},
